579d5ca091705ad74f5a2b2fa0ea94a01f77c3e6
arnabanimesh <arnabanimesh.a@gmail.com>
2020-10-13 23:41:05 +0530

    Add fps counter.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,26 @@
+extern crate web_sys;
+use web_sys::console;
+
 mod utils;
 
 use wasm_bindgen::prelude::*;
+
+pub struct Timer<'a> {
+    name: &'a str,
+}
+
+impl<'a> Timer<'a> {
+    pub fn new(name: &'a str) -> Timer<'a> {
+        console::time_with_label(name);
+        Timer { name }
+    }
+}
+
+impl<'a> Drop for Timer<'a> {
+    fn drop(&mut self) {
+        console::time_end_with_label(self.name);
+    }
+}
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -56,6 +76,8 @@ impl Universe {
     }
 
     pub fn tick(&mut self) {
+        // let _timer = Timer::new("Universe::tick");
+
         let mut next = self.cells.clone();
 
         for row in 0..self.height {
