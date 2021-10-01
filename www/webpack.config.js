8c33141039fa43f5599b71141c93d52d5fea71e2
Arnab Animesh Das <arnabanimesh.a@gmail.com>
2021-10-01 09:07:29 +0530

    Implement minify and compress while building the node app

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,5 +1,7 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
+var BrotliPlugin = require('brotli-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: "./bootstrap.js",
@@ -8,7 +10,15 @@ module.exports = {
     filename: "bootstrap.js",
   },
   mode: "development",
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
   plugins: [
-    new CopyWebpackPlugin(['index.html', 'favicon.ico'])
+    new CopyWebpackPlugin(['index.html', 'favicon.ico']),
+    new BrotliPlugin({
+      asset: '[file].br',
+      test: /\.(js|wasm)$/
+    })
   ],
 };
